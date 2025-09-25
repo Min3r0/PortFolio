643d04d50fdefa5c1c90a4d1e8555f9397a7b6cd
Min3r0 <romainauge31@gmail.com>
2025-09-25 18:36:28 +0200

    update Diplome  add school name

--- js/diplomes.js
+++ js/diplomes.js
@@ -1,23 +1,23 @@
-fetch("txt/diplomes.txt")
+fetch("text/diplomes.txt")
   .then(response => response.text())
   .then(data => {
     const container = document.getElementById("etudeContent");
     const lignes = data.trim().split("\n");
 
     lignes.forEach((ligne, index) => {
-      const [date, titre] = ligne.split("|").map(s => s.trim());
+      // On sépare en 3 parties
+      const [date, diplome, ecole] = ligne.split("|").map(s => s.trim());
 
-      // Création du bloc diplôme
       const article = document.createElement("article");
       article.classList.add("etude_item");
       article.innerHTML = `
         <h3 class="etude_date">${date}</h3>
-        <h2 class="etude">${titre}</h2>
+        <h2 class="etude">${diplome}</h2>
+        <p class="etude_ecole">${ecole}</p>
       `;
 
       container.appendChild(article);
 
-      // Ligne de séparation sauf après le dernier
       if (index < lignes.length - 1) {
         const separator = document.createElement("div");
         separator.classList.add("ligne");
