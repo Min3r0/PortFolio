5a06c877e724d479768db1dc77c249ff9b96cd64
Minero <116750793+Min3r0@users.noreply.github.com>
2026-03-23 20:22:38 +0100

    Delete js/about_me.js

--- js/about_me.js
+++
@@ -1,7 +0,0 @@
-fetch('txt/aboutme.txt')
-  .then(response => response.text())
-  .then(text => {
-    // Remplace les sauts de ligne par <br> pour conserver la mise en page
-    document.getElementById('about_me').innerHTML = text.replace(/\n/g, '<br>');
-  })
-  .catch(error => console.error('Erreur lors du chargement du texte :', error));
